f2b708b2efd91baa8f69ccfd8fe1961b4eadb304
nicholas-stull <your-email@example.com>
2024-09-14 15:18:10 -0400

    finished proj 2-1

---
+++ Chapter02/project01/proj2-01.md
@@ -0,0 +1,19 @@
+# Project Review Document
+
+# HTML 
+## Changes
+project02-01.html:
+
+Author: Nick Stull
+Date:   09.13.2024
+
+Github Repo: tinkernerd/ISYS115
+
+Filename: index.htm
+
+```js
+<script src="project02-01.js" defer></script>
+```
+
+# JS
+
--- Chapter02/project01/project02-01.js
+++ Chapter02/project01/project02-01.js
@@ -3,9 +3,38 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Nick Stull
+      Date:   09.14.2024
+      
+      Github Repo: tinkernerd/ISYS115
 
       Filename: project02-01.js
  */
 
+// Function to convert Fahrenheit to Celsius
+function FahrenheitToCelsius(degree) {
+    return (degree - 32) / 1.8;
+}
+
+// Function to convert Celsius to Fahrenheit
+function CelsiusToFahrenheit(degree) {
+    return (degree * 1.8) + 32;
+}
+
+// Adding onchange event handler to the element with id "cValue"
+document.getElementById('cValue').onchange = function() {
+    // Get the value of the element with id "cValue"
+    var cDegree = parseFloat(document.getElementById('cValue').value);
+    
+    // Set the value of the element with id "fValue" to the converted temperature
+    document.getElementById('fValue').value = CelsiusToFahrenheit(cDegree);
+};
+
+// Adding onchange event handler to the element with id "fValue"
+document.getElementById('fValue').onchange = function() {
+    // Get the value of the element with id "fValue"
+    var fDegree = parseFloat(document.getElementById('fValue').value);
+    
+    // Set the value of the element with id "cValue" to the converted temperature
+    document.getElementById('cValue').value = FahrenheitToCelsius(fDegree);
+};
